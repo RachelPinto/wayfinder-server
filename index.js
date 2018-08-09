d073b1ffcb40c7582baff64b9398147febc28115
rpinto <rachel.lea.pinto@gmail.com>
2018-08-08 22:00:29 -0700

    begin adding JWT auth

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const localStrategy = require('./passport/local');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
+const jwtStrategy = require('./passport/jwt');
 // const {dbConnect} = require('./db-knex');
 
 const recordsRouter = require('./routes/records');
@@ -35,7 +36,7 @@ app.use(express.json());
 app.use('/api', authRouter);
 
 passport.use(localStrategy);
-
+passport.use(jwtStrategy);
 
 // Mount routers
 app.use('/api/records', recordsRouter);
--- routes/records.js
+++ routes/records.js
@@ -3,6 +3,7 @@
 const express = require('express');
 
 const router = express.Router();
+router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
 
 const Record = require('../models/records');
 
